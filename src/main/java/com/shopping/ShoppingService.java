14b6f6f14caba2b5bd61b85dd04c2292ce559812
TCSUnited <UnitedTCS@Uniteds-iMac.local>
2017-05-22 17:38:08 +0530

    test

---
+++ src/main/java/com/shopping/ShoppingService.java
@@ -0,0 +1,51 @@
+package com.shopping;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping(value="/rest/getFlights")
+public class ShoppingService {
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value="/",method = RequestMethod.GET)
+	public String getFlights() throws IOException{
+		System.out.println("******************** Inside Request ******************");
+		
+		String url = "http://localhost:4984/sync_gateway/getFlights";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+	
+		return response.toString();
+	}
+}
